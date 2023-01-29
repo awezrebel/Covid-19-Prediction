238ad32e940269d6c3e91e9dfb276901c58ffc44
Shaik Awez <59769777+awezrebel@users.noreply.github.com>
2023-01-29 20:59:22 +0530

    Update notification.py

--- notification.py
+++ notification.py
@@ -11,7 +11,7 @@ client= Client(account_sid, auth_token)
 message = client.messages.create( 
 body='Thank you for using covid predictor your id is : ' + str(id), 
 from_='+18646614447', 
-to = '+916303731463' 
+to = 'mobilenumber' 
 )
 
 print(message.sid) 
